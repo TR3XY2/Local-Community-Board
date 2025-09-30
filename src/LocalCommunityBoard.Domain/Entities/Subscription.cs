3fbfe3fbd8cc423799329782c996d8779336cade
Yuriy Fundak <yfundak@gmail.com>
2025-09-30 19:31:47 +0300

    add entities for db

---
+++ src/LocalCommunityBoard.Domain/Entities/Subscription.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LocalCommunityBoard.Domain.Entities
+{
+    [Table("Subscriptions")]
+    public class Subscription
+    {
+        [Column("id")]
+        [Key] 
+        public int Id { get; set; }
+
+        [Column("user_id")]
+        [Required] 
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        [Column("location_id")]
+        [Required] 
+        public int LocationId { get; set; }
+        public Location Location { get; set; } = null!;
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
