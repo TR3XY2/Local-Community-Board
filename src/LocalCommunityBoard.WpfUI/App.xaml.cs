f8928f280950632af25220cfb5e03fd53c1a9e4f
Yuriy Fundak <yfundak@gmail.com>
2025-09-30 22:49:15 +0300

    add logging when starting or exiting application

--- src/LocalCommunityBoard.WpfUI/App.xaml.cs
+++ src/LocalCommunityBoard.WpfUI/App.xaml.cs
@@ -1,13 +1,32 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Serilog;
 
-namespace LocalCommunityBoard.WpfUI;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application
+namespace LocalCommunityBoard.WpfUI
 {
-}
+    public partial class App : Application
+    {
+        public App()
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
 
+            Log.Information("Application starting up...");
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Log.Information("WPF application started");
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Log.Information("Application is shutting down");
+            Log.CloseAndFlush();
+            base.OnExit(e);
+        }
+    }
+}
