8f4943a2981fa2ec6563c3dfa0c86bffe6a99b18
Yuriy Fundak <yfundak@gmail.com>
2025-10-08 00:24:28 +0300

    move tests to src folder

---
+++ src/LocalCommunityBoard.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace LocalCommunityBoard.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //test
+            Assert.Equal(1, 1);
+        }
+    }
+}
